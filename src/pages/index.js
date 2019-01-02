908683730624451cbe1b8465603a4938d95ddb51
yungpanko <jrjohns@mit.edu>
2019-01-01 19:47:49 -0500

    color

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ export default class IndexPage extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-              <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+              <h1 className="has-text-weight-bold is-size-2">Latest Content</h1>
             </div>
             {posts
               .map(({ node: post }) => (
@@ -33,7 +33,7 @@ export default class IndexPage extends React.Component {
                     {post.excerpt}
                     <br />
                     <br />
-                    <Link className="button is-small" to={post.fields.slug}>
+                    <Link className="button is-small is-primary is-outlined" to={post.fields.slug}>
                       Keep Reading →
                     </Link>
                   </p>
