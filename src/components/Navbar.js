662e55ebdb2702672595b0e19eee8302df881ee0
yungpanko <jrjohns@mit.edu>
2019-01-03 17:04:13 -0500

    logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,7 +35,7 @@ const Navbar = class extends React.Component {
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item" title="Logo">
-          <img src={logo} alt="logo" style={{ width: 'height' }} />
+          <img className="rotate" src={logo} alt="logo" style={{ width: 'height' }} />
         </Link>
         <Link to="/" className="navbar-item" title="jared-johnson">
           Jared Ray Johnson
